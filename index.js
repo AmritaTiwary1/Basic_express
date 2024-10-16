abda5eddbdb1af0c6d330658bb9408882f55754c
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-10-16 19:30:28 +0530

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { name } = require('ejs');
+const express = require('express');
+const app = express();
+
+app.set("view engine","ejs");
+app.use(express.static('./public'));
+
+app.get('/',(req,res)=>{
+    res.render("script",{name:''})
+})
+
+app.listen(3000);
