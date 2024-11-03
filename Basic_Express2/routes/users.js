ce37b6412d5c116ec9f9c327e35d60a0ffa6dcdd
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-11-03 23:25:59 +0530

    connecting mongoose to mongodb,making table/collection , making schema and exporting it

--- Basic_Express2/routes/users.js
+++ Basic_Express2/routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+//install mongoosejs
+//require and setup connection
+//make schema
+//create model and export
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+const mongoose = require('mongoose');  
+
+mongoose.connect('mongodb://127.0.0.1:27017/sampleDatabase');
+
+
+//making schema so that we can use it in mode(collection)
+
+const userSchema = mongoose.Schema({
+    username:String,
+    name:String,
+    age:Number
+})
+
+module.exports = mongoose.model("user",schema); //user is name of the collecton in db & it should be written inside inverted comma,here schema is userSchema
+ 
+
+//Another way ---
+
+// const mongoose=require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/flipkart')
+
+// module.exports = mongoose.model("Product", mongoose.Schema({
+//     productId:Number,
+//     productName:String,
+//     productPrice:Number
+// }))
+
+
+
