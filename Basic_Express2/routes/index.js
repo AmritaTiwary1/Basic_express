9c8173e1031bcd49b31a296f41fa477877610e34
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-11-03 23:32:07 +0530

    importing usermodel from user.js

--- Basic_Express2/routes/index.js
+++ Basic_Express2/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('./users');  //importing model from user.js where we have exported  
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
   //res.send("hi")
 });
+
+router.get("/create")
 
 module.exports = router;
